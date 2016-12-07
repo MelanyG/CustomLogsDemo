e6cb0279ed94fc96faa3d9f3c5dc2f8e8072f819
Melany Gulianovych <gmr@inbox.ru>
2016-12-07 21:02:32 +0200

    Initial Commit

---
+++ CustomLogsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomLogsDemo
+//
+//  Created by Melany Gulianovych on 12/7/16.
+//  Copyright © 2016 Melany Gulianovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomLogsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomLogsDemo
+//
+//  Created by Melany Gulianovych on 12/7/16.
+//  Copyright © 2016 Melany Gulianovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
